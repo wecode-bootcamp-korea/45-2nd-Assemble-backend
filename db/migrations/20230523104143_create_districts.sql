66b380d91869c7d8c1080810f16d1ade0435bb67
하지현 <hazzi@hajihyeon-ui-MacBookAir.local>
2023-05-24 10:50:46 +0900

    MODIFY: 변수명 변경

--- db/migrations/20230523104143_create_districts.sql
+++ db/migrations/20230523104143_create_districts.sql
@@ -2,7 +2,7 @@
 CREATE TABLE districts (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     district VARCHAR(50) NOT NULL,
-    district_image VARCHAR(400) NOT NULL
+    image_url VARCHAR(400) NOT NULL
 )
 -- migrate:down
 DROP TABLE districts;
