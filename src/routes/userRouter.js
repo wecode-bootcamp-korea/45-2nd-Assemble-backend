d1008f29e29aa945be02e624404433699035c655
하지현 <hazzi@hajihyeon-ui-MacBookAir.local>
2023-05-25 15:47:39 +0900

    ADD: User 정보 불러오는 API

--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,7 +3,10 @@ const userController = require('../controllers/userController');
 
 const router = express.Router();
 
+const { validateToken } = require('../middlewares/auth');
+
 router.get('/kakaologin', userController.kakaologin);
+router.get('', validateToken, userController.getUserById);
 
 module.exports = {
   router,
