93eaa14f2e80c708a4316755cb70ff6fb2c28afa
jinss5 <jinss1145@gmail.com>
2023-05-26 19:55:04 +0900

    ADD: cancel reservation scheduler

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 require('dotenv').config();
 
 const { createApp } = require('./app.js');
+const reservationService = require('./src/services/reservationService');
 const { dataSource } = require('./src/models/dataSource');
+const { scheduleJob } = require('node-schedule');
+
+const cancelReservation = () => {
+  scheduleJob('0 0 0 * * *', async () => {
+    await reservationService.cancelReservation();
+  });
+};
 
 const startServer = async () => {
   try {
@@ -17,7 +25,10 @@ const startServer = async () => {
         console.log('Error occurred during Data Source initialization', err);
         dataSource.destroy();
       });
-    app.listen(PORT, () => console.log(`Server is listening on ${PORT}`));
+    app.listen(PORT, () => {
+      console.log(`Server is listening on ${PORT}`);
+      cancelReservation();
+    });
   } catch (err) {
     console.error(err);
   }
